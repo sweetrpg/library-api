bda73192b763014a217e4cc450d8e49009d59b14
Paul Schifferer <paul@schifferers.net>
2021-08-17 19:26:31 -0700

    Trigger a build

--- Sources/App/Constants.swift
+++ Sources/App/Constants.swift
@@ -1,6 +1,9 @@
 //
-// Constants.swift
-// Copyright (c) 2021 Paul Schifferer.
+//  Constants.swift
+//  Library API
+//
+//  Created by Paul Schifferer on 4/22/17.
+//  Copyright 2021 SweetRPG. All rights reserved.
 //
 
 import Foundation

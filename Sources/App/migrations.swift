f69021649070b80ea2f4c94643d55faa1d78a395
Paul Schifferer <paul@schifferers.net>
2021-08-17 20:38:37 -0700

    Trigger a build

--- Sources/App/migrations.swift
+++ Sources/App/migrations.swift
@@ -1,5 +1,9 @@
 //
-// Created by Paul Schifferer on 6/16/21.
+//  migrations.swift
+//  Library API
+//
+//  Created by Paul Schifferer on 4/22/17.
+//  Copyright 2021 SweetRPG. All rights reserved.
 //
 
 import Vapor

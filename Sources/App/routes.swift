98b1b92d200410e4605d4ddf95aa5f2eb6a442ed
Paul Schifferer <paul@schifferers.net>
2021-08-17 20:51:12 -0700

    Trigger another build

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,6 +1,9 @@
 //
-// routes.swift
-// Copyright (c) 2021 Paul Schifferer.
+//  routes.swift
+//  Library API
+//
+//  Created by Paul Schifferer on 4/22/17.
+//  Copyright 2021 SweetRPG. All rights reserved.
 //
 
 import Fluent

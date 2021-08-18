981b3332f94a4f2de4381e8fe06490bb03ce91c1
Paul Schifferer <paul@schifferers.net>
2021-08-17 18:44:31 -0700

    Trigger a build

--- Sources/App/Controllers/VolumesController.swift
+++ Sources/App/Controllers/VolumesController.swift
@@ -3,7 +3,7 @@
 //  Library API
 //
 //  Created by Paul Schifferer on 4/22/17.
-//  Copyright © 2021 SweetRPG. All rights reserved.
+//  Copyright 2021 SweetRPG. All rights reserved.
 //
 
 import Fluent

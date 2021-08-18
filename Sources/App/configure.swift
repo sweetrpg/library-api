6b0a7d8b8d4a712aa95b1ba284728b8c7b1f8c57
Paul Schifferer <paul@schifferers.net>
2021-08-17 20:56:47 -0700

    Trigger yet another build

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -14,8 +14,6 @@ import Common
 
 
 public func configure(_ app : Application) throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     app.middleware.use(app.sessions.middleware)
     app.middleware.use(LogRequestMiddleware())
 

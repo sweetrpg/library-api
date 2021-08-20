770fd25b756f9099a686c0f6349a21d38ac1b3ce
Paul Schifferer <paul@schifferers.net>
2021-08-19 18:03:52 -0700

    Play it again, Sam.

--- Sources/App/Models/LibraryHealthInfo.swift
+++ Sources/App/Models/LibraryHealthInfo.swift
@@ -10,11 +10,19 @@ import Vapor
 import Common
 
 
+/**
+ */
 struct LibraryHealthInfo : HealthInfo, Content {
+/**
+ */
     let timestamp : Date
+/**
+ */
     let dbHealthy : Bool
 }
 
+/**
+ */
 func healthCallback() -> LibraryHealthInfo {
     return LibraryHealthInfo(timestamp: Date(), dbHealthy: true)
 }

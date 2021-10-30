a782d66dfbf7eab31821ddfc40209f0f14e391d1
Paul Schifferer <paul@schifferers.net>
2021-10-30 09:33:33 -0700

    Clean up entrypoint script

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-set -x
 set -e
 
-printenv
-#pwd
-#ls -la
-#ls -la /config
-
 #newrelic-admin run-program gunicorn wsgi:app
-#gunicorn wsgi:app
-uwsgi --http :8281 --module wsgi:app --master --processes 4 --threads 2
-#python appserver.py
-#sleep 3600
+uwsgi --http :${PORT} --module wsgi:app --master --processes 4 --threads 2

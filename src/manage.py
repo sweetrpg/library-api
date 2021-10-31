6220cbb9b8db0161c025cddf13e0b370e798345b
Paul Schifferer <paul@schifferers.net>
2021-10-30 21:56:25 -0700

    Trigger a new docker build.

--- src/manage.py
+++ src/manage.py
@@ -10,15 +10,6 @@ from flask_migrate import Migrate, MigrateCommand
 
 from sweetrpg_library_api.application.main import create_app
 from sweetrpg_library_api.application.db import db
-# from application.models.common.game_system import GameSystem, GameSystemFacetDatum, GameSystemImageDatum
-# from application.models.initiative.condition import Condition, ConditionHealthAdjustment
-# from application.models.initiative.encounter import Encounter, EncounterParticipant, EncounterParticipantGroup, EncounterParticipantHealthDatum, EncounterParticipantMetrics, EncounterParticipantTurnData, EncounterRegion, EncounterSession, EncounterSessionTimelineEntry
-# from application.models.initiative.group import EncounterGroup
-# from application.models.initiative.participant import Participant, ParticipantGroup, ParticipantHealthDatum
-# from application.models.initiative.tracked_encounter import TrackedEncounter
-# from application.models.user import User, Identity, Role, Permission, UserRole
-# from application.models.profile import Profile
-# from application.models.entitlement import Entitlement, EntitlementGrant
 
 app = create_app()
 
@@ -33,17 +24,7 @@ manager.add_command('db', MigrateCommand)
 
 @manager.shell
 def shell_ctx():
-    return dict(app=app,
-                db=db
-                # User=User, Identity=Identity, Role=Role, Profile=Profile, Permission=Permission, UserRole=UserRole,
-                # Entitlement=Entitlement, EntitlmentGrant=EntitlementGrant,
-                # GameSystem=GameSystem, GameSystemFacetDatum=GameSystemFacetDatum, GameSystemImageDatum=GameSystemImageDatum,
-                # Condition=Condition, ConditionHealthAdjustment=ConditionHealthAdjustment,
-                # Encounter=Encounter, EncounterParticipant=EncounterParticipant, EncounterParticipantGroup=EncounterParticipantGroup, EncounterParticipantHealthDatum=EncounterParticipantHealthDatum, EncounterParticipantMetrics=EncounterParticipantMetrics, EncounterParticipantTurnData=EncounterParticipantTurnData, EncounterRegion=EncounterRegion, EncounterSession=EncounterSession, EncounterSessionTimelineEntry=EncounterSessionTimelineEntry,
-                # EncounterGroup=EncounterGroup,
-                # Participant=Participant, ParticipantGroup=ParticipantGroup, ParticipantHealthDatum=ParticipantHealthDatum,
-                # TrackedEncounter=TrackedEncounter
-                )
+    return dict(app=app, db=db)
 
 
 if __name__ == '__main__':

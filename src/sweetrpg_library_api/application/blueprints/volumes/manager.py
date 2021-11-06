fc061e3bbcbfe2bcfa89ce180121e29d4a107bb6
Paul Schifferer <paul@schifferers.net>
2021-11-06 14:56:47 -0700

    Trigger another build.

--- src/sweetrpg_library_api/application/blueprints/volumes/manager.py
+++ src/sweetrpg_library_api/application/blueprints/volumes/manager.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 __author__ = "Paul Schifferer <dm@sweetrpg.com>"
-"""
+"""Resource definitions for volumes.
 """
 
 from flask_rest_jsonapi import ResourceList, ResourceDetail, ResourceRelationship

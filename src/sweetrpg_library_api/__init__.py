68a94da2283f2ad9793cae2d01d7e8ca9c0be3a9
Paul Schifferer <paul@schifferers.net>
2021-11-14 09:51:50 -0800

    Remove redundant author item.

--- src/sweetrpg_library_api/__init__.py
+++ src/sweetrpg_library_api/__init__.py
@@ -8,7 +8,6 @@ __description__ = "API microservice for SweetRPG Library"
 __url__ = "https://sweetrpg.com"
 __version__ = "0.0.38"
 __build__ = 0x000011
-__author__ = "Paul Schifferer"
 __author_email__ = "dm@sweetrpg.com"
 __license__ = "MIT"
 __copyright__ = "Copyright 2021 SweetRPG"

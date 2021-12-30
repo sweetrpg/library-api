585dd4ef9adbf087ef3718f956fecdc7f091a024
Paul Schifferer <paul@schifferers.net>
2021-12-30 14:29:10 -0800

    Add log format

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,4 +3,10 @@
 set -e
 
 #newrelic-admin run-program gunicorn wsgi:app
-uwsgi --http :${PORT} --module wsgi:app --master --processes 4 --threads 2
+uwsgi \
+    --logformat '%(addr) - %(user) [%(ltime)] "%(method) %(uri) %(proto)" %(status) %(size) "%(referer)" "%(uagent)"' \
+    --http :${PORT} \
+    --module wsgi:app \
+    --master \
+    --processes 4 \
+    --threads 2

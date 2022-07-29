d72da2b1bb658347baf7f813b56cb2e9a030d614
Paul Schifferer <paul@schifferers.net>
2022-07-28 18:20:55 -0700

    Trigger build

--- src/sweetrpg_library_api/__init__.py
+++ src/sweetrpg_library_api/__init__.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 __author__ = "Paul Schifferer <dm@sweetrpg.com>"
-"""Root.
+"""
+__init__.py
+Root.
 """
 
 __title__ = "sweetrpg-library-api"
 __description__ = "API microservice for SweetRPG Library"
 __url__ = "https://sweetrpg.com"
 __version__ = "0.0.129"
-__build__ = 0x000011
+__build__ = 0x000012
 __author_email__ = "dm@sweetrpg.com"
 __license__ = "MIT"
 __copyright__ = "Copyright 2022 SweetRPG"
